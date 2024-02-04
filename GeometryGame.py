993a1405b4d657af629270a7abf61bc2576088db
Akash Samant <109782459+akash150298@users.noreply.github.com>
2024-02-04 15:51:15 +0530

    Update GeometryGame.py
    importing turtle library for visualizing rectangle and point
    Inheritance to create child class guirectangle that will inherit property of rectange
    Inheritance to create gui Point

--- GeometryGame.py
+++ GeometryGame.py
@@ -1,4 +1,5 @@
 from random import randint
+import turtle
 
 class Point:
 
@@ -7,23 +8,54 @@ class Point:
     self.y = y
 
   def falls_in_rectangle(self, rectangle):
-    if rectangle.leftUp.x < self.x \
-    < rectangle.rightUp.x \
-    and rectangle.leftUp.y < self.y \
-    < rectangle.rightUp.y:
+    if rectangle.point1.x < self.x \
+    < rectangle.point2.x \
+    and rectangle.point1.y < self.y \
+    < rectangle.point2.y:
       return True
     else:
       return False
 
 class Rectangle:
-  def __init__(self, leftUp, rightUp):
-    self.leftUp = leftUp
-    self.rightUp = rightUp
+  def __init__(self, point1, point2):
+    self.point1 = point1
+    self.point2  = point2
 
-rectangle = Rectangle(Point(randint(0,9), randint(0,9)), Point(randint(10,17), randint(10,17) ))
+class GuiRectangle(Rectangle):
+  def draw(self, canvas):
+    canvas.penup()
+    canvas.goto(self.point1.x, self.point1.y)
+    canvas.pendown()
+    canvas.forward(self.point2.x - self.point1.x)
+    canvas.left(90)
+    canvas.forward(self.point2.y - self.point1.y)
+    canvas.left(90)
+    canvas.forward(self.point2.x - self.point1.x)
+    canvas.left(90)
+    canvas.forward(self.point2.y - self.point1.y)
 
-print("Rectangle Coordinates: ", rectangle.leftup.x, "," rectangle.leftup.y, "and", rectangle.rightup.x, "," rectangle.rightup.y)
+class GuiPoint(Point):
+  def draw(self, canvas, size = 5, color = 'red'):
+    canvas.penup()
+    canvas.goto(self.x, self.y)
+    canvas.pendown()
+    canvas.dot(size, color)
 
-user_point = Point(float(input("Guess x : ")), float(input("Guess y : ")))
+rectangle = GuiRectangle(Point(randint(0,400),randint(0,400)), Point(randint(10,400), randint(10,400)))
 
-print("The point was inside the rectangle", user_point.falls_in_rectangle(rectangle))
+print("Rectangle Coordinates: ",
+      rectangle.point1.x, ",",
+      rectangle.point1.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
+
+user_point = GuiPoint(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
+
+myturtle = turtle.Turtle()
+rectangle.draw(canvas=myturtle)
+user_point.draw(canvas=myturtle)
+turtle.done()
